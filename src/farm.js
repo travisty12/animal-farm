0a1337d8cb373b63389eb62d57a279f8d643e0d0
Nate Wessels and Travis Toal <travis@>
2019-03-19 16:35:39 -0700

    add other animals / resources, more advanced hunger dynamics

--- src/farm.js
+++ src/farm.js
@@ -24,33 +24,70 @@ export class Animal {
       return true;
     }
   }
-  feed(amount) {
+  feed(amount, animal) {
     let that = this;
     return function(food) {
       that.hunger += amount;
-      return `The horse ate the ${food}! ${amount} hunger is restored.`;
+      return `The ${animal} ate the ${food}! ${amount} hunger is restored.`;
     };
   }
 }
 
 export class Farm {
   constructor() {
-    this.animal = [];
+    this.horses = [];
+    this.pigs = [];
+    this.chickens = [];
+    this.sheeps = [];
     this.food = 10;
     this.foodRate = 0;
     this.hungry = [];
+    this.hungryChicken = [];
+    this.hungrySheep = 0;
+    this.truffles = 0;
+    this.trufflesRate = 0;
+    this.eggs = 0;
+    this.eggsRate = 0;
+    this.woolRate = 0;
+    this.wool = 0;
   }
   animalFed(amount) {
     this.food -= amount;
   }
   addAnimal(animal) {
-    this.animal.push(animal);
+    this.horses.push(animal);
+  }
+  addPig(animal) {
+    this.pigs.push(animal);
+  }
+  addChicken(animal) {
+    this.chickens.push(animal);
+  }
+  addSheep(animal) {
+    this.sheeps.push(animal);
   }
   removeAnimal(i) {
-    this.animal.splice(i, 1);
+    this.horses.splice(i, 1);
     this.hungry.splice(i, 1);
     this.food +=10;
     this.foodRate += 1;
+  }
+  removePig() {
+    this.pigs.splice(0, 1);
+    this.food +=10;
+    this.foodRate += 3;
+  }
+  removeChicken() {
+    this.chickens.splice(0, 1);
+    this.food +=10;
+    this.foodRate += 1;
+
+  }
+  removeSheep() {
+    this.sheeps.splice(0, 1);
+    this.food +=10;
+    this.foodRate += 2;
+
   }
 
   randomEvent() {
@@ -62,30 +99,166 @@ export class Farm {
     }
   }
 
-  findAnimal(animal) {
-    let length = this.animal.length;
+  findHorse(animal) {
+    let length = this.horses.length;
     for(let i = 0; i < length; i++) {
-      if (this.animal[i].name == animal) {
-        return this.animal[i];
+      if (this.horses[i].name == animal) {
+        return this.horses[i];
+      }
+    }
+  }
+  findPig(animal) {
+    let length = this.pigs.length;
+    for(let i = 0; i < length; i++) {
+      if (this.pigs[i].name == animal) {
+        return this.pigs[i];
+      }
+    }
+  }
+  findChicken(animal) {
+    let length = this.chickens.length;
+    for(let i = 0; i < length; i++) {
+      if (this.chickens[i].name == animal) {
+        return this.chickens[i];
+      }
+    }
+  }
+
+  findSheep(animal) {
+    let length = this.sheeps.length;
+    for(let i = 0; i < length; i++) {
+      if (this.sheeps[i].name == animal) {
+        return this.sheeps[i];
       }
     }
   }
   feedAnimal(animals, food, amount) {
-    let animal = this.findAnimal(animals);
-    let feeder = animal.feed(amount);
+    let animal = this.findHorse(animals);
+    let feeder = animal.feed(amount, "Horse");
     this.food -= amount;
     return (feeder(food));
   }
+
+  feedPig(animals, food, amount) {
+    let animal = this.findPig(animals);
+    let feeder = animal.feed(amount, "Pig");
+    this.food -= amount;
+    return (feeder(food));
+  }
+  feedChicken(animals, food, amount) {
+    let animal = this.findChicken(animals);
+    let feeder = animal.feed(amount, "Chicken");
+    this.food -= amount;
+    return (feeder(food));
+  }
+
+  feedSheep(animals, food, amount) {
+    let animal = this.findSheep(animals);
+    let feeder = animal.feed(amount, "Sheep");
+    this.food -= amount;
+    return (feeder(food));
+  }
+
+
   horseMaker(i, hunger) {
     let horse = new Animal("Horse" + i, hunger);
     this.addAnimal(horse);
-    this.animal[i].setHunger();
+    this.horses[i].setHunger();
     this.foodRate += 2;
   }
 
+  pigMaker(i, hunger) {
+    let pig = new Animal("Pig" + i, hunger);
+    this.addPig(pig);
+    this.foodRate -= 3;
+    this.trufflesRate += 1;
+  }
+  chickenMaker(i, hunger) {
+    let chicken = new Animal("Chicken" + i, hunger);
+    this.addChicken(chicken);
+    this.foodRate -= 1;
+    this.eggsRate += 1;
+  }
+
+  sheepMaker(i, hunger) {
+    let sheep = new Animal("Sheep" + i, hunger);
+    this.addSheep(sheep);
+    this.foodRate -= 2;
+    this.woolRate += 1;
+  }
+
+  checkFood(starving) {
+    if (this.food <= 0) {
+      this.food = 0;
+      if (!starving) {
+        console.log(starving);
+        if (this.pigs[0]) {
+          if (this.chickens[0]) {
+            this.eggsRate *= -2;
+          }
+          if (this.sheeps[0]) {
+            this.woolRate *= -2;
+          }
+          if (!(this.sheeps[0] || this.chickens[0])) {
+            this.trufflesRate *= -1;
+          }
+        } else {
+          if (this.chickens[0]) {
+            this.eggsRate *= -1;
+          }
+          if (this.sheeps[0]) {
+            this.woolRate *= -1;
+          }
+        }
+
+        starving = true;
+      }
+      if (this.eggs <= 0) {
+        if (this.chickens[0]) {
+          this.removeChicken();
+          this.eggsRate = 0;
+
+        }
+      }
+      if (this.wool <= 0) {
+        if (this.sheeps[0]) {
+          this.removeSheep();
+          this.woolRate = 0;
+
+        }
+      }
+      if (!(this.chickens[0] || this.sheeps[0]) && this.pigs[0] && this.truffles <= 0) {
+        this.removePig();
+        this.trufflesRate = 0;
+      }
+    } else {
+      if (starving) {
+        if (this.pigs[0]) {
+          if (this.chickens[0]) {
+            this.eggsRate /= -2;
+          }
+          if (this.sheeps[0]) {
+            this.woolRate /= -2;
+          }
+        } else {
+          if (this.chickens[0]) {
+            this.eggsRate *= -1;
+          }
+          if (this.sheeps[0]) {
+            this.woolRate *= -1;
+          }
+        }
+        starving = false;
+      }
+    }
+    return starving;
+  }
+
   checkHunger() {
-    for (let i = 0; i < this.animal.length; i++) {
-      if (this.animal[i].hunger == 0) {
+    let anyHungry;
+    for (let i = 0; i < this.horses.length; i++) {
+      if (this.horses[i].hunger == 0) {
+        anyHungry = true;
         if (!this.hungry[i]) {
           // console.log(this.hungry)
           this.foodRate -= 3;
@@ -93,7 +266,7 @@ export class Farm {
           $("#skull").show();
 
         }
-        this.animal[i].hunger += 1;
+        this.horses[i].hunger += 1;
       } else {
         if (this.hungry[i]) {
           this.foodRate += 3;
@@ -101,6 +274,9 @@ export class Farm {
 
         }
       }
+    }
+    if(anyHungry != true) {
+      $("#skull").hide();
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -9,22 +9,37 @@ $(document).ready(function() {
     $("#intro").fadeOut();
     setTimeout(function() {
       $("#game").fadeIn();
+      let starving = false;
       setInterval(function() {
         farm.food += farm.foodRate;
+        farm.truffles += farm.trufflesRate;
+        farm.eggs += farm.eggsRate;
+        farm.wool += farm.woolRate;
         farm.checkHunger();
+        starving = farm.checkFood(starving);
         $("#foodPile").text(farm.food);
         $("#foodRate").text(farm.foodRate);
-        if (farm.animal[0]) {
-          for (let i = 0; i < farm.animal.length; i++) {
-            if(farm.animal[i].isHungry()) {
+        $("#truffles").text(farm.truffles);
+        $("#trufflesRate").text(farm.trufflesRate);
+        $("#eggs").text(farm.eggs);
+        $("#eggsRate").text(farm.eggsRate);
+        $("#wool").text(farm.wool);
+        $("#woolRate").text(farm.woolRate);
+
+        if (farm.horses[0]) {
+          for (let i = 0; i < farm.horses.length; i++) {
+            if(farm.horses[i].isHungry()) {
               $("#horse" + i + "Hunger").css("color", "red");
             } else {
               $("#horse" + i + "Hunger").css("color", "black");
             }
-            $("#horseNum").text(farm.animal.length);
-            $("#horse" + i + "Hunger").text(`${farm.animal[i].hunger}`);
+            $("#horse" + i + "Hunger").text(`${farm.horses[i].hunger}`);
           }
+          $("#horseNum").text(farm.horses.length);
         }
+        $("#pigNum").text(farm.pigs.length);
+        $("#chickenNum").text(farm.chickens.length);
+        $("#sheepsNum").text(farm.sheeps.length);
       }, 1000);
     }, 400);
   });
@@ -33,24 +48,48 @@ $(document).ready(function() {
   });
   let z = 0;
   $("#create").click(function() {
-    if (farm.food >= 1 * (z + 1)) {
-      farm.horseMaker(z, 10);
-      farm.food -= (z + 1) * 1;
+    if (farm.food >= (20 + (20 * z))) {
+      farm.horseMaker(z, 50);
+      farm.food -= (20 + (20 * z));
       $("#hungerHolder").append("<span id='horse" + z + "Hunger'></span> ");
       z++;
     }
   });
+  let w = 0;
+  $("#newPig").click(function() {
+    if (farm.food >= 1 * (w + 1)) {
+      farm.pigMaker(w, 10);
+      farm.food -= (w + 1) * 1;
+      w++;
+    }
+  });
+  let c = 0;
+  $("#newChicken").click(function() {
+    if (farm.food >= 1 * (c + 1)) {
+      farm.chickenMaker(c, 10);
+      farm.food -= (c + 1) * 1;
+      c++;
+    }
+  });
+  let d = 0;
+  $("#newSheeps").click(function() {
+    if (farm.food >= 1 * (d + 1)) {
+      farm.sheepMaker(d, 10);
+      farm.food -= (d + 1) * 1;
+      d++;
+    }
+  });
   $("#feed").click(function() {
-    if (farm.animal[0] && farm.food >= 5 * farm.animal.length) {
-      for (let j = 0; j < farm.animal.length; j++) {
+    if (farm.horses[0] && farm.food >= 5 * farm.horses.length) {
+      for (let j = 0; j < farm.horses.length; j++) {
         $(".output").text(farm.feedAnimal("Horse" + j, "carrot", 5));
         // $(".horseCollect").text(farm.collectFood("Horse" + j, "carrote", 10))
       }
-      $(".output").append(`You used ${5*farm.animal.length} food to feed your horses</p>`);
+      $(".output").append(`You used ${5*farm.horses.length} food to feed your horses</p>`);
     }
   });
   $("#skull").click(function() {
-    for (let j = farm.animal.length - 1; j >= 0; j--) {
+    for (let j = farm.horses.length - 1; j >= 0; j--) {
       if (farm.hungry[j]) {
         farm.removeAnimal(j);
         z--;
@@ -59,15 +98,14 @@ $(document).ready(function() {
     }
     $("#hungerHolder").html("");
     let newHunger = [];
-    for (let y = farm.animal.length - 1; y >= 0; y--) {
-      let hunger = farm.animal[y].hunger;
+    for (let y = farm.horses.length - 1; y >= 0; y--) {
+      let hunger = farm.horses[y].hunger;
       newHunger[y] = hunger;
-      console.log(hunger);
     }
-    for (let x = farm.animal.length - 1; x >= 0; x--) {
+    for (let x = farm.horses.length - 1; x >= 0; x--) {
       $("#hungerHolder").append("<span id='horse" + x + "Hunger'></span> ");
 
-      farm.removeAnimal(farm.animal[x]);
+      farm.removeAnimal(farm.horses[x]);
       farm.foodRate = 0;
 
     }
